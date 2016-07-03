291bca90cfb544716ba7b2c57f08f77b4aa4233a
Akshay Mankar <itsakshaymankar@gmail.com>
2016-07-04 00:08:56 +1000

    Handle failure scenario, in case where a dependency doesn't exist in the input

--- src/main/scala/DependencyResolver.scala
+++ src/main/scala/DependencyResolver.scala
@@ -1,6 +1,14 @@
+
 object DependencyResolver {
+  sealed trait DependencyResolution
+  case class ResolutionSuccess(executionSet: List[Set[Feature]]) extends DependencyResolution
+  object ResolutionFailure extends DependencyResolution
+
   def resolve(features: Set[Feature], dependencies: Set[Dependency]) = {
-    resolveLoop(features, dependencies, List())._3
+    if(dependencies.map(_.to).diff(features).isEmpty)
+      ResolutionSuccess(resolveLoop(features, dependencies, List())._3)
+    else
+      ResolutionFailure
   }
 
   private def resolveLoop(features: Set[Feature],
--- src/test/scala/DependencyResolver$Test.scala
+++ src/test/scala/DependencyResolver$Test.scala
@@ -1,3 +1,4 @@
+import DependencyResolver._
 import org.specs2.mutable.Specification
 
 class DependencyResolver$Test extends Specification with ImplicitHelpers {
@@ -9,7 +10,7 @@ class DependencyResolver$Test extends Specification with ImplicitHelpers {
       val features = Set(a,b,c)
       val dependencies = Set.empty[Dependency]
 
-      DependencyResolver.resolve(features, dependencies) mustEqual List(Set(a,b,c))
+      DependencyResolver.resolve(features, dependencies) mustEqual ResolutionSuccess(List(Set(a,b,c)))
     }
 
     "resolve things when there are simple dependencies" in {
@@ -19,16 +20,21 @@ class DependencyResolver$Test extends Specification with ImplicitHelpers {
       val features = Set(a,b,c)
       val dependencies = Set(Dependency(a,b), Dependency(b,c))
 
-      DependencyResolver.resolve(features, dependencies) mustEqual List(Set(c), Set(b), Set(a))
+      DependencyResolver.resolve(features, dependencies) mustEqual ResolutionSuccess(List(Set(c), Set(b), Set(a)))
     }
 
     "resolve the example" in {
-      val group1 = FeatureParser.parseItem("(A,B,C,G,H)[G->A,H->A,H->B]").getOrElse((Set.empty[Feature], Set.empty[Dependency]))
-      val group2 = FeatureParser.parseItem("(D,E,F,I,J)[I->D,I->E,J->F,J->I,I->H]").getOrElse((Set.empty[Feature], Set.empty[Dependency]))
+      val group1 = FeatureParser.parseItem("(A,B,C,G,H)[G->A,H->A,H->B]").get
+      val group2 = FeatureParser.parseItem("(D,E,F,I,J)[I->D,I->E,J->F,J->I,I->H]").get
 
       val expectedOrder = List("A,B,C,D,E,F".toFs, "G,H".toFs, "I".toFs, "J".toFs)
 
-      DependencyResolver.resolve(group1._1 ++ group2._1, group1._2 ++ group2._2) mustEqual expectedOrder
+      DependencyResolver.resolve(group1._1 ++ group2._1, group1._2 ++ group2._2) mustEqual ResolutionSuccess(expectedOrder)
+    }
+
+    "should fail, if unresolvable" in {
+      val group = FeatureParser.parseItem("(A,B)[A->D]").get
+      DependencyResolver.resolve(group._1, group._2) mustEqual ResolutionFailure
     }
   }
 }
